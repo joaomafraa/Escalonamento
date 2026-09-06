26a087eafd11678416202b6f2d54c221aae2f4a1
joaomafraa <jcmafraneto@gmail.com>
2026-09-06 17:01:45 -0300

    feat: implementada a funcao de ler tarefas

--- scheduler.c
+++ scheduler.c
@@ -6,11 +6,17 @@
 
 typedef struct tarefas{
     char nome[50];
+    int periodo;
+    int deadline;
+    int burst;
+
+    int restante;
+    long long prazo;
 }tarefas;
 
 int validar_args(int argc,char*argv[]){
     if(argc !=3){
-        fprintf(stderr,"Erro: argumentos insuficientes\n");
+        fprintf(stderr,"Erro: quantidade incorreta de argumentos\n");
         return 0;
     }
     if(strcmp(argv[1],"rate") != 0 && strcmp(argv[1],"edf") != 0){
@@ -20,7 +26,7 @@ int validar_args(int argc,char*argv[]){
     return 1;
 }
 
-int ler_arq(char *nome){
+int ler_arq(char *nome,int *tempo_total){
     FILE * arq;
     arq = fopen(nome,"r");
     if(arq == NULL){
@@ -67,11 +73,69 @@ int ler_arq(char *nome){
         fclose(arq);
         return 0;
     }
+    *tempo_total=(int)valor;
     fclose(arq);
     return 1;
 }
+
+int ler_tarefa(char *linha, tarefas *tarefa){
+    char *campos[4];
+    char *parte = strtok(linha," \t\r\n");
+    int quantidade = 0;
+
+    while (parte!=NULL && quantidade<4) {
+        campos[quantidade++] = parte;
+        parte=strtok(NULL, " \t\r\n");
+    }
+
+    if(quantidade != 4 || parte!=NULL){
+        fprintf(stderr, "Erro: esperado NOME PERIODO DEADLINE BURST.\n");
+        return 0;
+    }
+
+    if (strlen(campos[0]) >= sizeof(tarefa->nome)) {
+        fprintf(stderr, "Erro: nome muito longo.\n");
+        return 0;
+    }
+
+    int valores[3];
+
+    for (int i=0; i<3;i++) {
+        char *fim;
+        errno=0;
+        long valor=strtol(campos[i+1], &fim,10);
+
+        if (fim == campos[i+1] ||errno == ERANGE || *fim != '\0' || valor <= 0 || valor > INT_MAX){
+            fprintf(stderr, "Erro: numero invalido.\n");
+            return 0;
+        }
+
+        valores[i]=(int)valor;
+    }
+    if(valores[2]>valores[1] || valores[1]>valores[0]){
+        fprintf(stderr, "Erro: tempos inconsistentes.\n");
+        return 0;
+    }
+    strcpy(tarefa->nome,campos[0]);
+    tarefa->periodo=valores[0];
+    tarefa->deadline=valores[1];
+    tarefa->burst=valores[2];
+    tarefa->restante=0;
+    tarefa->prazo=0;
+    return 1;
+}
 int main(int argc, char*argv[]){
+    char linha[] = "ATT 20 12 8";
+    tarefas tarefa;
+    if (ler_tarefa(linha, &tarefa) == 0) {
+        return 1;
+    }
+    printf("Nome: %s\n",tarefa.nome);
+    printf("Periodo: %d\n",tarefa.periodo);
+    printf("Deadline: %d\n",tarefa.deadline);
+    printf("Burst: %d",tarefa.burst);
+    int tempo=0;
     validar_args(argc,argv);
-    ler_arq(argv[2]);
+    ler_arq(argv[2],&tempo);
     return 0;
 }   
